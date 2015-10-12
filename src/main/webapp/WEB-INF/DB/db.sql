84c6150992b43c04f2e4c100161d2ac44fb1fb41
roshanmutha <rcmutha@gmail.com>
2015-10-12 11:19:13 +0530

    SQL Schema changes

--- src/main/webapp/WEB-INF/DB/db.sql
+++ src/main/webapp/WEB-INF/DB/db.sql
@@ -1,4 +1,3 @@
-delimiter $$
 
 CREATE TABLE `transaction` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -8,9 +7,10 @@ CREATE TABLE `transaction` (
   `area` varchar(45) NOT NULL,
   `mobile` varchar(45) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8$$
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-delimiter $$
+
+
 
 CREATE TABLE `item` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -20,17 +20,22 @@ CREATE TABLE `item` (
   `available_count` int(11) NOT NULL,
   `created` datetime NOT NULL,
   `modified` datetime NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8$$
-
-delimiter $$
-
-CREATE TABLE `item_transaction` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `item_id` int(11) DEFAULT NULL,
-  `transaction_id` int(11) DEFAULT NULL,
+  `t_id` int(11) NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `transactionFKey` (`transaction_id`),
-  CONSTRAINT `transactionFKey` FOREIGN KEY (`transaction_id`) REFERENCES `transaction` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8$$
+  KEY `TransactionFKEY_idx` (`t_id`),
+  CONSTRAINT `TransactionFKEY` FOREIGN KEY (`t_id`) REFERENCES `transaction` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+--delimiter $$
+--
+--CREATE TABLE `item_transaction` (
+--  `id` int(11) NOT NULL AUTO_INCREMENT,
+--  `item_id` int(11) DEFAULT NULL,
+--  `transaction_id` int(11) DEFAULT NULL,
+--  PRIMARY KEY (`id`),
+--  KEY `transactionFKey` (`transaction_id`),
+--  CONSTRAINT `transactionFKey` FOREIGN KEY (`transaction_id`) REFERENCES `transaction` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+--) ENGINE=InnoDB DEFAULT CHARSET=utf8$$
 
